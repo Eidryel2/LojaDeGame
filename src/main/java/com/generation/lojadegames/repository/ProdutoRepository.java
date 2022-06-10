c42997042797061b040d6825c635b2b27a03baa4
Edryel2 <adrielly70@hotmail.com>
2022-06-10 09:03:53 -0300

    Correção do erro

--- src/main/java/com/generation/lojadegames/repository/ProdutoRepository.java
+++ src/main/java/com/generation/lojadegames/repository/ProdutoRepository.java
@@ -14,7 +14,7 @@ public interface ProdutoRepository extends JpaRepository<Produto, Long>{
 	 
 	 
 	
-	public List<Produto> findAllByNomeContainingIgnoreCase1(@Param("nome") String nome);
+	
 	 
 	 
 	
